179afb6ba527e1a835301b357df2a4194783d40e
XiaoMing <56507353+15960049901@users.noreply.github.com>
2021-04-29 15:30:07 +0800

    新建get请求
    get请求获取数据库信息

--- routes/logins.js
+++ routes/logins.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
+router.get('/', async function (req, res, next) {
+  var logins = await models.Login.findAll();
+  res.json({ logins: logins });
+});
 
 module.exports = router;
