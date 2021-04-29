da9136a3da6cb88fc58d78bfbfbbbb5c4b72552b
XiaoMing <56507353+15960049901@users.noreply.github.com>
2021-04-29 18:33:57 +0800

    实现登入功能
    修改get请求为由传参的get请求

--- routes/logins.js
+++ routes/logins.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var Op = models.Sequelize.Op
 
 router.get('/', async function (req, res, next) {
-  var logins = await models.Login.findAll();
-  res.json({ logins: logins });
+  // 搜索
+  var where = {};
+
+  // 查询用户名
+  var username = req.query.username;
+  if (username) {
+    where.username = {
+      [Op.like]: username
+    }
+  }
+
+  var logins = await models.Login.findAll({
+    where: where
+  });
+
+  res.json({ date: logins });
 });
 
 module.exports = router;
